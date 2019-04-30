00ebecb3b5c41f358a70fb908d11e3132f43fa9e
christopherwilco001 <>
2019-04-30 14:22:14 -0400

    Err bug resolved

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://thawing-castle-12546.herokuapp.com',
+  production: 'https://thawing-castle-12546.herokuapp.com/',
   development: 'http://localhost:4741/'
 }
 
